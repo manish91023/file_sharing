3db014a1e0df6e8b148aa792f315b3f4caa05717
manish kumar <manishgga09@gmail.com>
2025-02-18 17:45:20 +0530

    error solve

--- Client/fileshare/src/main.jsx
+++ Client/fileshare/src/main.jsx
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const clientId =import.meta.env.VITE_CLIENT_ID
-const domain = import.meta.env.VITE_CLIENT_DOMAIN
+const clientId ="WFI1UVKrmyQzdFmmxEH4sNiOK8S8gly1"
+const domain = "manishgga09.us.auth0.com"
 const audiences=import.meta.env.VITE_CLIENT_AUDIENCE
 
 
@@ -16,7 +16,7 @@ createRoot(document.getElementById("root")).render(
       clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience:`${audiences}`
+        audience:`https://manishgga09.us.auth0.com/api/v2/`
       }}
     >
       <App />
